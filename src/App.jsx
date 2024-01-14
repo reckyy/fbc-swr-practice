7fffcd54bf9720b97a7f920a6edab21ee3daf8ad
reckyy <srreki98@gmail.com>
2024-01-14 16:27:40 +0900

    💯 test: prettier

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import "./App.css";
 import useSWR from "swr";
 
 const headers = { Accept: "application/json" };
-const fetcher = url => fetch(url, { headers }).then(r => r.json());
+const fetcher = (url) => fetch(url, { headers }).then((r) => r.json());
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
 
   const { data, error } = useSWR(url, fetcher);
 
-  if(error) return <div>Failed to load.</div>;
-  if(!data) return <div>Loading...</div>;
+  if (error) return <div>Failed to load.</div>;
+  if (!data) return <div>Loading...</div>;
   return <div>Status: {data.description}</div>;
 }
 
